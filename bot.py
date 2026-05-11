ffc36c1f233cea8c1c2153087bf417b65ae1ec96
Smarking-jpg1 <gsamar552@gmail.com>
2026-05-12 01:15:34 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -39,7 +39,7 @@ if not MONGODB_URI:
 DATABASE_NAME = os.getenv("DATABASE_NAME", "attack_bot")
 API_URL = os.getenv("API_URL")
 API_KEY = os.getenv("API_KEY")
-ADMIN_IDS = [int(id.strip()) for id in os.getenv("ADMIN_IDS", "1793697840").split(",")]
+ADMIN_IDS = [int(id.strip()) for id in os.getenv("ADMIN_IDS", "5048841681").split(",")]
 
 # Blocked ports (must match backend)
 BLOCKED_PORTS = {8700, 20000, 443, 17500, 9031, 20002, 20001}
